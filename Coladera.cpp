ed932a8b39bb8cc5c3bec5df0308b232c1a74bbd
Laurad19 <54084034+Laurad19@users.noreply.github.com>
2019-10-01 17:52:35 -0500

    2da versión.
    Coladera jsdknf

--- Coladera.cpp
+++ Coladera.cpp
@@ -1,1 +1,52 @@
-#include
+#include<iostream>
+
+using namespace std;
+
+void llenaruno(int vec[], int n);
+void coladera(int vec[], int n);
+void mostrar(int vec[], int n);
+
+int main()
+{
+	int vec[1000];
+	llenaruno(vec,1000);
+	coladera(vec,1000);
+	mostrar(vec,1000);
+	
+	get.cin;
+	get.cin;
+	}
+void llenaruno(int vec[], int n)
+{
+	for(int i=1;i<=n; i++)
+	{
+		vec[i]=1;
+	}
+}
+void coladera(int vec[], int n)
+{
+	for(int i=2;i<n; i++)
+	{
+		if(vec[i]==1)
+			for(int j=i+1;j<n;j++)
+			{
+				if(j%i==0)
+				vec[j]=0;
+			}
+	}
+}
+void mostrar(int vec[], int n )
+{
+	for(int i=0;i<n; i++)
+	{
+		if(vec[i]==1)
+		{
+		cout<<i<<"\n";
+		}
+	}
+
+	}
+	
+	
+	
+	
